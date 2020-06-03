af5ff2585f496a5aa55393c34382dc8c166f2b0d
divyapratapsingh4011 <divypratapsingh13@gmail.com>
2020-06-03 18:25:33 +0530

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.indigo,
           accentColor: Colors.amber,
         ),
-        home: ,
+        home: Scaffold(),
       ),
     );
   }
