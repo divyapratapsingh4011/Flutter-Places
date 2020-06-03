8cfbbeb4afd8cd17554321bf8760d1f35beeaffe
divyapratapsingh4011 <divypratapsingh13@gmail.com>
2020-06-03 18:22:14 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Places
+
+A Practice Flutter project illustrating use of Native device features and SQLite.
---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(),
+        FlatButton(onPressed: () {}, child: null),
+      ],
+    );
+  }
+}
